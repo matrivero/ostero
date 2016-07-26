75d1c655c51dbc27c147b5fe797c79ab00d79006
matrivero <matiasr83@gmail.com>
2016-07-26 11:36:05 +0200

    housekeeping and some compilation improvements for the develop stuff

--- develop/compile.sh
+++ develop/compile.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-#generate signature file
+#remove previous and re-generate signature file
+rm -f external.pyf
 f2py -m external -h external.pyf external.f90
-#generate the object files (-fPIC option is mandatory)
+
+#remove previous and re-generate the object files (-fPIC option is mandatory)
+#in marenostrum you will need ifort instead of gfortran
+rm -f external.o demo_subroutine.o
 gfortran -c -fPIC external.f90 demo_subroutine.f90
+#ifort -c -fPIC external.f90 demo_subroutine.f90
+
 #link all
+rm -f external.so
 f2py -c external.pyf *.o
